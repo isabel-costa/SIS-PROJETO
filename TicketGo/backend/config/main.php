3813ee16e8cfc7acd72d13d972f20ba3e5629381
isabel-costa <2231698@my.ipleiria.pt>
2024-12-10 12:44:49 +0000

    API e Controlador de Eventos

--- TicketGo/backend/config/main.php
+++ TicketGo/backend/config/main.php
@@ -11,7 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'api' => [
+            'class' => 'backend\modules\api\ModuleAPI',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
@@ -37,14 +41,16 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'class' => 'yii\rest\UrlRule',
+                'controller' => 'api/evento',
+                'pluralize' => true,
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
